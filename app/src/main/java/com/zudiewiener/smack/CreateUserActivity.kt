8ff25d264a2b78039544e68e0acfae528b70155c
Poldi <leo@zudiewiener.com>
2021-02-07 10:54:04 +1100

    Added Create User Screens

---
+++ app/src/main/java/com/zudiewiener/smack/CreateUserActivity.kt
@@ -0,0 +1,24 @@
+package com.zudiewiener.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View){
+
+    }
+
+    fun generateBackgroundColourClicked(view: View){
+
+    }
+
+    fun createUserClicked(view: View){
+
+    }
+}
--- app/src/main/java/com/zudiewiener/smack/LoginActivity.kt
+++ app/src/main/java/com/zudiewiener/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.zudiewiener.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,7 +16,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserBtnClicked(view: View){
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 
 }
