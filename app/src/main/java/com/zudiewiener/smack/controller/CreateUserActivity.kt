55609b11b7e4b8e9f901a81dc7117dc6004fefbf
Poldi <leo@zudiewiener.com>
2021-02-09 16:55:14 +1100

    Cannot continue as LocalBroadcastManager is deprecated and don't have to learn the replacement stuff until I need it

--- app/src/main/java/com/zudiewiener/smack/controller/CreateUserActivity.kt
+++ app/src/main/java/com/zudiewiener/smack/controller/CreateUserActivity.kt
@@ -1,12 +1,15 @@
 package com.zudiewiener.smack.controller
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.zudiewiener.smack.R
 import com.zudiewiener.smack.services.AuthService
 import com.zudiewiener.smack.services.UserDataService
+import com.zudiewiener.smack.utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,6 +21,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view: View){
@@ -55,22 +59,55 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View){
+        enableSpinner(true)
         val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
-        AuthService.registerUser(this,email, password){registerSuccess ->
-            if (registerSuccess){
-                AuthService.loginUser(this,email,password){loginSuccess ->
-                    if (loginSuccess){
-                       AuthService.createUser(this,userName,email,userAvatar, avatarColor) { createSuccess ->
-                            if (createSuccess){
-                                finish()
+
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
+            AuthService.registerUser(this,email, password){registerSuccess ->
+                if (registerSuccess){
+                    AuthService.loginUser(this,email,password){loginSuccess ->
+                        if (loginSuccess){
+                            AuthService.createUser(this,userName,email,userAvatar, avatarColor) { createSuccess ->
+                                if (createSuccess){
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    // LocalBroadcastManager.ge
+                                    enableSpinner(false)
+                                    finish()
+                                } else {
+                                    errorToast()
+                                }
                             }
-                       }
+                        } else {
+                            errorToast()
+                        }
                     }
+                } else {
+                    errorToast()
                 }
             }
+        } else {
+            Toast.makeText(this, "Make sure all fields are filled", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
+        }
+
+    }
+
+    fun errorToast(){
+        Toast.makeText(this, "Something went wrong in the user creation", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean){
+        if (enable){
+            createSpinner.visibility = View.VISIBLE
+        } else {
+            createSpinner.visibility = View.INVISIBLE
 
         }
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        generateBackgroundColBtn.isEnabled = !enable
     }
 }
